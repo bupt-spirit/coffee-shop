aebc0dbc865211bcdf7567e5d0171c4a98a6de7b
Lin Yinfeng <lin.yinfeng@outlook.com>
2018-09-17 17:35:51 +0800

    Add add staff

--- coffee-shop-main/src/main/java/coffeeshop/ejb/StoreManagerBean.java
+++ coffee-shop-main/src/main/java/coffeeshop/ejb/StoreManagerBean.java
@@ -2,6 +2,7 @@ package coffeeshop.ejb;
 
 import coffeeshop.entity.Store;
 import coffeeshop.facade.StoreFacade;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -10,19 +11,28 @@ import javax.ejb.Stateless;
 @Stateless
 public class StoreManagerBean implements StoreManager {
 
-    
     @EJB
     private StoreFacade storeFacade;
-    
+
     @Override
-    public void addStore(String country, String province, String district, String city, String detail){
-        Store newStore = new Store(null, country, province, city, district, detail, (short)1);
+    public void addStore(String country, String province, String district, String city, String detail) {
+        Store newStore = new Store(null, country, province, city, district, detail, (short) 1);
+        newStore.setOrderInfoList(new ArrayList<>());
+        newStore.setStaffList(new ArrayList<>());
         storeFacade.create(newStore);
     }
-    
+
     @Override
-    public List<Store> getStores(){
+    public List<Store> getStores() {
         return storeFacade.findAll();
     }
 
+    @Override
+    public Store getStoreById(int id) throws StoreManagerException {
+        Store store = storeFacade.find(id);
+        if (store == null) {
+            throw new StoreManagerException("Store with id " + id + " not found");
+        }
+        return store;
+    }
 }
