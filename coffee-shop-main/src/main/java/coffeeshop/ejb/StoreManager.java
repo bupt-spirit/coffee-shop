e93ed449731d4ca8564d630af6e8382ec75d0807
Lin Yinfeng <lin.yinfeng@outlook.com>
2018-09-21 08:24:17 +0800

    Refactor admin add store

--- coffee-shop-main/src/main/java/coffeeshop/ejb/StoreManager.java
+++ coffee-shop-main/src/main/java/coffeeshop/ejb/StoreManager.java
@@ -4,8 +4,8 @@ import coffeeshop.entity.Store;
 import java.util.List;
 
 public interface StoreManager {
-    
-    Store addStore(String country, String province, String city, String district, String detail);
+
+    Store addStore(Store store);
     
     List<Store> getStores();
     
--- coffee-shop-main/src/main/java/coffeeshop/ejb/StoreManagerBean.java
+++ coffee-shop-main/src/main/java/coffeeshop/ejb/StoreManagerBean.java
@@ -17,17 +17,16 @@ public class StoreManagerBean implements StoreManager {
 
     @EJB
     private UserManager userManager;
-    
+
     @EJB
     private OrderManager orderManager;
 
     @Override
-    public Store addStore(String country, String province, String city, String district, String detail) {
-        Store newStore = new Store(null, country, province, city, district, detail, (short) 1);
-        newStore.setOrderInfoList(new ArrayList<>());
-        newStore.setStaffList(new ArrayList<>());
-        storeFacade.create(newStore);
-        return newStore;
+    public Store addStore(Store store) {
+        store.setOrderInfoList(new ArrayList<>());
+        store.setStaffList(new ArrayList<>());
+        storeFacade.create(store);
+        return store;
     }
 
     @Override
@@ -58,5 +57,5 @@ public class StoreManagerBean implements StoreManager {
             throw new StoreManagerException("Unfinish order exist");
         }
     }
-    
+
 }
